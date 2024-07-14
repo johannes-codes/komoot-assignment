d7ebfb99faf041f7bf7882624c8b71c3c9a807df
Johannes Meissner <johannes.meissner@hse.com>
2024-07-14 13:32:50 +0200

    Initial Commit

---
+++ Komoot Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Komoot Assignment
+//
+//  Created by Meißner, Johannes, HSE DE on 14.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Komoot Assignment/Komoot_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Komoot_AssignmentApp.swift
+//  Komoot Assignment
+//
+//  Created by Meißner, Johannes, HSE DE on 14.07.24.
+//
+
+import SwiftUI
+
+@main
+struct Komoot_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
